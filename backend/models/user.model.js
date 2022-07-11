c3a4489970b32de58465b36f28fdbe0c413cbdc9
antran1245 <antran1245@gmail.com>
2022-07-10 23:40:30 -0700

    config and user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: [true, "Username already in used"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [3, "Password is at least 3 characters long."]
+    },
+    email : {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email already used"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("Users", UserSchema);
+model.exports = User;
