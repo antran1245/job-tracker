c3d8f9494fa6421a18ccb81e1e093ba932ac93bf
antran1245 <antran1245@gmail.com>
2022-07-13 00:14:55 -0700

    basic input for log in and reg form

--- client/src/components/LoginRegister.jsx
+++ client/src/components/LoginRegister.jsx
@@ -1,18 +1,44 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Form} from 'react-bootstrap';
 
 export default function LoginRegister() {
     return (
         <Container>
             <Row>
                 <Col xs={12} lg={6}>
-                    <div className='box'>
-                        login
-                    </div>
+                    <Form className='box'>
+                        <h2>Log In</h2>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Email Address:</Form.Label>
+                            <Form.Control type='text' placeholder='Enter email'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type='text' placeholder='Enter password'/>
+                        </Form.Group>
+                        <Button variant='primary' type='submit'>Submit</Button>
+                    </Form>
                 </Col>
                 <Col>
-                    <div className='box'>
-                        register
-                    </div>
+                    <Form className='box'>
+                        <h2>Sign Up</h2>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type='text' placeholder='Enter username'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type='text' placeholder='Enter email'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='text' placeholder='Enter password'/>
+                        </Form.Group>
+                        <Form.Group className='mb-3'>
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type='text' placeholder='Confirm password'/>
+                        </Form.Group>
+                        <Button variant='primary' type='submit'>Submit</Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
