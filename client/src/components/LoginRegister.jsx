7e4d8388c8ee8701ba1deb8fb40c169cf96a712f
antran1245 <antran1245@gmail.com>
2022-07-12 23:54:46 -0700

    install bootstrap and working on login and reg

--- client/src/components/LoginRegister.jsx
+++ client/src/components/LoginRegister.jsx
@@ -1,3 +1,20 @@
+import {Container, Row, Col} from 'react-bootstrap';
+
 export default function LoginRegister() {
-    
+    return (
+        <Container>
+            <Row>
+                <Col xs={12} lg={6}>
+                    <div className='box'>
+                        login
+                    </div>
+                </Col>
+                <Col>
+                    <div className='box'>
+                        register
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
