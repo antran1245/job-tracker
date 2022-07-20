0762d23346b30ac386451afa8ec7038a9e8cfe75
antran1245 <antran1245@gmail.com>
2022-07-19 21:34:29 -0700

    added a cookie to login and register

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,34 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
 
 module.exports.login = async(req, res) => {
     const {email, password} = req.body;
     try {
+        // Find a user by email
         const user = await User.findOne({email: email})
+
+        // Valid password by compare a hash password with an unhash password
         const validPassword = await bcrypt.compare(password, user.password);
         if(validPassword) {
-            res.json({message: 'ok', user: user});
+
+            // A set of fields to include in the token
+            const payload = {
+                _id: user._id,
+                username: user.username
+            }
+            // Generate the token
+            const userToken = jwt.sign(payload, process.env.SECRET_KEY);
+            
+            // Set the cookie with a name of "usertoken"
+            res.cookie("usertoken", userToken, process.env.SECRET_KEY, {
+                httpOnly: true
+            })
+
+            // response message to client
+            .json({message: 'ok', user: {_id: user.id, username: user.username}});
         } else {
             res.status(400).json({message: "Invalid Email/Password"})
         }
@@ -19,13 +40,26 @@ module.exports.login = async(req, res) => {
 module.exports.register = async(req, res) => {
     const {username, password, email} = req.body;
     try {
+        // Create a user account to the mongoDB
         let user = await User.create({
             username,
             password,
             email
         })
-        res.json(user)
+
+        // A set of fields to include in the token
+        const payload = {
+            _id: user._id,
+            username: user.username,
+        }
+        // Generate a token
+        const userToken = jwt.sign(payload , process.env.SECRET_KEY)
+        res.cookie('usertoken', userToken, process.env.SECRET_KEY, {
+            httpOnly: true
+        })
+        .json({message: "ok", user: {_id: user.id, username: user.username}})
     } catch (err) {
         res.json(err)
     }
 }
+
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -22,5 +23,12 @@ const UserSchema = new mongoose.Schema({
     }
 }, {timestamps: true})
 
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+    .then(hash => {
+        this.password = hash;
+        next();
+    })
+})
 const User = mongoose.model("Users", UserSchema);
 module.exports = User;
