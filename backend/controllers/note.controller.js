41c663e2cafdbf42cee9f7c3226a86ef2f9a5132
antran1245 <antran1245@gmail.com>
2022-08-30 12:13:44 -0700

    note push to arrays and added a userId

--- backend/controllers/note.controller.js
+++ backend/controllers/note.controller.js
@@ -1,15 +1,26 @@
 const Note = require("../models/note.model");
 const User = require("../models/user.model");
 
-module.exports.createNote = (req, res) => {
-    Note.create(req.body)
-    .then(resp => res.json(resp))
-    .catch(err => res.json(err))
+module.exports.createNote = async(req, res) => {
+    const {userId, jobId, interviewNote, note} = req.body
+    const exist = await Note.exists({jobId: jobId, userId: userId})
+    if (exist) {
+        Note.findOneAndUpdate(
+            {jobId: jobId},
+            {$push: {interviewNote: interviewNote, note: note}},
+            new: true)
+        .then(resp => res.json(resp))
+        .catch(err => res.json(err))
+    } else {
+        Note.create(req.body)
+        .then(resp => res.json(resp))
+        .catch(err => res.json(err))
+    }
 }
 
 module.exports.findNote = (req, res) => {
-    const {_id, _jobId} = req.params
-    User.findOne({_id: _id})
+    const {userId, jobId} = req.params
+    Note.findOne({jobId: jobId, userId: userId})
     .then(resp => console.log(resp))
     .catch(err => console.log(err))
 }
--- backend/models/note.model.js
+++ backend/models/note.model.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Types.ObjectId
+    },
     jobId: {
         type: mongoose.Types.ObjectId
     },
